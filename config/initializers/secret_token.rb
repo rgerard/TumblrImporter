6a158c34c8202e9c00d44f3940f7d4faa63a2cad
rgerard <ryan.gerard@gmail.com>
2011-08-07 15:50:06 -0700

    Initial project creation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TumblrImporter::Application.config.secret_token = '9f13101d5bec669d55351561d01f41f81fb038095f92cbefc3971a67891192661f8b49344d818190a701b33ca0cee1dd6ecd0d48b9bac0213c7c7d518d0efc9a'
